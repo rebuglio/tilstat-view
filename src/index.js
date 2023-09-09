9ff87c13b40c83a9cf16528f407c9cb004f66ae5
rebuglio <m.rebuglio@gmail.com>
2023-09-09 23:54:08 +0200

    version 2.0

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './global.css';
+import App from './app';
+import {BrowserRouter as Router} from 'react-router-dom';
+import CookieBanner from "./pages/cookie-banner";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <CookieBanner />
+        <Router basename="/tilstat">
+            <App/>
+        </Router>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
